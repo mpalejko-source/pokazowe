64a534fc397eae6e2399fa295cc50f5745f493a8
mpalejko-source <acegrouppl@gmail.com>
2026-06-04 15:17:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ace-agro-v2';
+const CACHE_NAME = 'ace-agro-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,15 +7,14 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
